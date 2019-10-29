08d713d42f4a1d8f19a22820458de1faf9a290d9
kubo-hide-kun <hide44041@gmail.com>
2019-10-29 09:41:08 +0900

    Restore git tracking

---
+++ README.md
@@ -0,0 +1,6 @@
+## 参考文献
+[Play framework 1.2.5 ドキュメント](https://www.playframework.com/documentation/ja/1.2.x/home)
+
+
+実行できない場合はeclipse/appname.launchから下記を削る
+ -Djava.endorsed.dirs=&quot;C:\Users\intern1\Documents\play-1.5.3/framework/endorsed&quot;
---
+++ app/controllers/Application.java
@@ -0,0 +1,19 @@
+package controllers;
+
+import java.util.List;
+
+import models.Post;
+import play.mvc.Controller;
+
+public class Application extends Controller {
+
+    public static void index() {
+        Post frontPost = Post.find("order by postedAt desc").first();
+        System.out.println(frontPost);
+        List<Post> olderPosts = Post.find(
+            "order by postedAt desc"
+        ).from(1).fetch(10);
+        render(frontPost, olderPosts);
+    }
+
+}
