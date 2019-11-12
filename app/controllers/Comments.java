7819867ebb25f9716940e4019e4ba9cbbdbbc92d
kubo-hide-kun <hide44041@gmail.com>
2019-11-12 16:59:14 +0900

    [WIP]Create admin page

--- app/controllers/Comments.java
+++ app/controllers/Comments.java
@@ -1,4 +1,6 @@
 package controllers;
+import play.mvc.With;
 
+@With(Secure.class)
 public class Comments extends CRUD {
 }
--- app/controllers/Users.java
+++ app/controllers/Users.java
@@ -1,4 +1,6 @@
 package controllers;
+import play.mvc.With;
 
+@With(Secure.class)
 public class Users extends CRUD {
 }
