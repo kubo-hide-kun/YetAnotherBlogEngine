b70883b1118ac1b885d3f14c88f60bfc22ee30c2
kubo-hide-kun <hide44041@gmail.com>
2019-11-12 11:17:49 +0900

    Create tag class

---
+++ app/models/Tag.java
@@ -0,0 +1,26 @@
+package models;
+
+import play.db.jpa.Model;
+
+public class Tag extends Model implements Comparable<Tag>{
+	public String name;
+	private Tag(String name) {
+		this.name = name;
+	}
+
+	public String toString() {
+		return name;
+	}
+
+	public int compareTo(Tag otherTag) {
+		return name.compareTo(otherTag.name);
+	}
+
+	public static Tag findOrCreateByName(String name) {
+		Tag tag = Tag.find("byName", name).first();
+		if(tag == null) {
+			tag = new Tag(name);
+		}
+		return tag;
+	}
+}
